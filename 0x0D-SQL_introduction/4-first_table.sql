d2c27a15811819faebd254f3d31898afeb8ab886
JesseAger <agerjesse6@gmail.com>
2024-03-12 18:27:41 -0400

    256

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,5 +1,5 @@
 -- creating a table with properties id and name
-CREATE TABLE IF NOT EXISTS first_table(
+CREATE TABLE IF NOT EXISTS first_table (
        id INT,
-       name VARCHAR
+       name VARCHAR(256)
 );
